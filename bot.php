b67bdef9a2e10f0187190ae4b00384a5172f1a07
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-03-16 13:00:14 +0700

    no message

--- bot.php
+++ bot.php
@@ -5,6 +5,9 @@
  * Date: 3/16/2017 AD
  * Time: 12:53 PM
  */
-echo "bot bot bot";
+//echo "bot bot bot";
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('IUh8y7kZ3U4iXhHRN/S48+0fUqRVePqGhlR2wWQ4YDwB17bZmGj6iKWbYjIVC8ZvYobiV0nbt6LT6GBFJWS07CY8D2L8wMK9lzJ379h18JrkMefeCAeOqC3+WBeBlZTjAw/DAvNV8SsnfToIr/9u/QdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1c1d6b241ba3c0294c3cefdd15ecea7f']);
+$response = $bot->replyText('<reply token>', 'hello!');
 
 ?>
