34e7f1fa2da5619b008385226e20adb8d651f840
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-03-16 13:10:07 +0700

    bot

--- bot.php
+++ bot.php
@@ -5,9 +5,20 @@
  * Date: 3/16/2017 AD
  * Time: 12:53 PM
  */
-//echo "bot bot bot";
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('IUh8y7kZ3U4iXhHRN/S48+0fUqRVePqGhlR2wWQ4YDwB17bZmGj6iKWbYjIVC8ZvYobiV0nbt6LT6GBFJWS07CY8D2L8wMK9lzJ379h18JrkMefeCAeOqC3+WBeBlZTjAw/DAvNV8SsnfToIr/9u/QdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1c1d6b241ba3c0294c3cefdd15ecea7f']);
-$response = $bot->replyText('<reply token>', 'hello!');
+
+$access_token = 'IUh8y7kZ3U4iXhHRN/S48+0fUqRVePqGhlR2wWQ4YDwB17bZmGj6iKWbYjIVC8ZvYobiV0nbt6LT6GBFJWS07CY8D2L8wMK9lzJ379h18JrkMefeCAeOqC3+WBeBlZTjAw/DAvNV8SsnfToIr/9u/QdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
 
 ?>
